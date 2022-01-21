523c0c893be3c98357dd349608cfae050d8482ef
Nagisa Ando <nagisa@hanayoku.com>
2022-01-20 18:05:53 -0600

    footer is made

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faYoutube, faInstagram} from '@fortawesome/free-brands-svg-icons'
 const Footer = ({blok}) => {
   return (
-    <footer className="py-6 lg:py-10">
-      <div className="container md:flex | mx-auto | px-5 md:px-10">
+    <footer className="py-6 lg:py-10 bg-pink">
+      <div className="container md:flex justify-between | mx-auto | px-5 md:px-10">
         <div>
           <img
             src={`${blok.body[1].image}/m/fit-in/70x70/`}
@@ -9,7 +11,19 @@ const Footer = ({blok}) => {
             className=""
           />
         </div>
-        <ul></ul>
+        <ul className="mt-8 md:mt-0 | flex gap-x-5">
+          <li className="">
+            <a href={blok.body[0].link_list[0].link.url} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="lg" style={{color: 'white'}} />
+            </a>
+          </li>
+
+          <li className="">
+            <a href={blok.body[0].link_list[1].link.url} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faYoutube} size="lg" style={{color: 'white'}} />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000',
+      pink: '#eed2d2',
+    },
     extend: {},
   },
   variants: {
