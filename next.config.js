6d5501f62b27c26c2602e8ad1dc85f91afb35f4c
Nagisa Ando <nagisa@hanayoku.com>
2022-01-30 19:57:28 -0800

    recovered function from recipi page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   reactStrictMode: false,
-  i18n: {
-    localeDetection: false,
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+
   images: {
     domains: ['a.storyblok.com'],
   },
